28da43a03b300ef0fdf4ade8a942d7957dea17cb
wanyvic <q873040807@gmail.com>
2018-12-07 10:22:04 +0800

    remove ping localhost

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-edge -d n2n -c $N2N_NAME -k $N2N_KEY -a $N2N_LOCALIP -l $N2N_SERVERIP
 sed -i 's/^PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config 
 service ssh restart ||service sshd restart
 mkdir /root/.ssh
 echo -e $SSH_PUBKEY"\n" > /root/.ssh/authorized_keys
-ping localhost
+edge -d n2n -c $N2N_NAME -k $N2N_KEY -a $N2N_LOCALIP -l $N2N_SERVERIP -f > /var/log/edge.log
 
