fd0f4f197916d02ba67ad748d8d092f1d6f30094
Wany <q873040807@gmail.com>
2019-07-11 16:19:48 +0800

    Update masternode_docker_install.sh

--- masternode_docker_install.sh
+++ masternode_docker_install.sh
@@ -14,9 +14,4 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
-  sudo apt-key add -
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update
+sudo docker version
